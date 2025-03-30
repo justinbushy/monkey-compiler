6f68508318f91032e3b49f87afd9a3a9455cfcbf
justinbushy <justinmbushphotography@gmail.com>
2025-03-30 09:30:43 -0600

    first commit - end point of interpreter

---
+++ repl/repl.go
@@ -0,0 +1,48 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParseErrors(out io.Writer, errros []string) {
+	for _, msg := range errros {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
